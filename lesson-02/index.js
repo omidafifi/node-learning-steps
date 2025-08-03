1433d9327c93159fedbf222878cb4a3ee00e8bd6
Omid Afifi <afifiomid@yahoo.com>
2025-08-03 23:25:12 +0330

    Switch from CommonJS to ES Modules with import/export syntax

--- lesson-02/index.js
+++ lesson-02/index.js
@@ -2,5 +2,8 @@
 //Es Modules => import , export , export defult
 // CommonJs Modules => require , module.exports.foo= .. , module.exportes
 
-const { add, subtract } = require("./commonJs-module");
-console.log(add(2, 5), subtract(5, 3));
+// const { add, subtract } = require("./commonJs-module");
+
+import Srt, { add, subtract } from "./es-modules.js";
+
+console.log(add(2, 5), subtract(5, 3), Srt);
