65eddc220836982c842b13ffa2ed207baab53806
Omid Afifi <afifiomid@yahoo.com>
2025-08-03 22:26:57 +0330

    feat: Demonstrate usage of global and globalThis objects in Node.js
    - Added setTimeout example to show asynchronous callback execution
    - Logged `global` and `globalThis` to illustrate their equivalence in Node.js runtime

--- lesson-01/index.js
+++ lesson-01/index.js
@@ -1,2 +1,15 @@
 //Initialize Node.js project with basic index.js and package.json
-console.log("Hello NodeJs");
+// console.log("Hello NodeJs");
+
+setTimeout(() => {
+  console.log("NodeJs Time out");
+}, 500);
+
+
+console.log(global);
+console.log(globalThis);
+//Both of them is ===
+//feat: Demonstrate usage of global and globalThis objects in Node.js
+
+// - Added setTimeout example to show asynchronous callback execution
+// - Logged `global` and `globalThis` to illustrate their equivalence in Node.js runtime
