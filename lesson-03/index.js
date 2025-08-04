505b970322dd019d336a10847197dc96d0aa708b
Omid Afifi <afifiomid@yahoo.com>
2025-08-04 19:10:55 +0330

    Nodemon

--- lesson-03/index.js
+++ lesson-03/index.js
@@ -10,3 +10,7 @@ http
   .listen(3000, () => {
     console.log("Server run on port 3000 : http://localhost:3000");
   });
+
+  //❓ Nodemon چیست؟
+
+// nodemon یک ابزار توسعه برای Node.js هست که کمک می‌کنه وقتی در فایل‌ها تغییری ایجاد می‌کنی، سرور اتوماتیک ری‌استارت بشه. دیگه لازم نیست هربار خودت Ctrl + C بزنی و دوباره node index.js بنویسی.
