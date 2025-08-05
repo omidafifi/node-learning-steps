592fc1d079e3e851e2be3081f7aaa52c00022dec
Omid Afifi <afifiomid@yahoo.com>
2025-08-05 14:24:36 +0330

    practice modules exportes in NodeJs

---
+++ lesson-02-practice/es-modules.js
@@ -0,0 +1,1 @@
+// module.exports = "This string for Test";
---
+++ lesson-02-practice/index.js
@@ -0,0 +1,5 @@
+//Export and imports
+
+const { add, destracter } = require("./common.modules");
+
+console.log(add(23, 45), destracter(38, 11));
