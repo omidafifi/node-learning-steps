11c32ef8a7872a2270e72bde19009ae0dfb9c634
Omid Afifi <afifiomid@yahoo.com>
2025-08-13 13:24:41 +0330

    new ObjectID

--- leson-MongoDB/delete.js
+++ leson-MongoDB/delete.js
@@ -1,4 +1,4 @@
-const { MongoClient, ObjectId } = require("mongodb");
+const { MongoClient, ObjectId, } = require("mongodb");
 const DB_URL = "mongodb://localhost:27017";
 const DB_Name = "mongodb-tutorials";
 const client = new MongoClient(DB_URL);
@@ -8,9 +8,10 @@ async function main() {
   const db = client.db(DB_Name);
   const userCollection = db.collection("user");
   //   const result = await userCollection.deleteOne({ firstName: "omidreza" });
-  const result = await userCollection.deleteOne({
-    _id: new ObjectId("689c5c1b803c00d4313b141f"),
-  });
-  console.log("Deleted Document => ", result);
+//   const result = await userCollection.deleteOne({
+//     _id: new ObjectId("689c5c1b803c00d4313b141f"),
+//   });
+//   console.log("Deleted Document => ", result);
 }
+console.log(new ObjectId("689c5c1b803c00d4313b141f"));
 main();
