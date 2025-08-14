a26ca30e5a3d311066d0f3cd054072ad0e9024c1
Omid Afifi <afifiomid@yahoo.com>
2025-08-14 21:23:01 +0330

    add basic HTML structure and click event for days section

--- bootcamp-todo-project/toDoList.js
+++ bootcamp-todo-project/toDoList.js
@@ -1,2 +1,13 @@
-const getInformation = "todays task";
-const daysTask = [""];
+
+const getInformation = document.getElementById("toDoList");
+console.log(getInformation);
+
+const daysTask = document.getElementById("days");
+console.log(daysTask);
+
+
+const handler = () => {
+  console.log("Clicked on days div");
+};
+
+daysTask.addEventListener("click", handler);
