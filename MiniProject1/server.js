82bb18c491cac69c1201798ba8f87c20cd926a3e
omidreza <afifiomid@yahoo.com>
2025-08-20 15:46:24 +0330

    +

--- MiniProject1/server.js
+++ MiniProject1/server.js
@@ -1,15 +1,18 @@
 const http = require("http");
-const PORT = 3000;
 const products = require("./data/products");
-const server = http
-  .createServer((req, res) => {
-    // => (Find )
-    if (req.url == "/api/products") {
-      ProductsController.get(req, res);
-    } else {
-      res.writeHead(404, { "Content-Type": "application/json" });
-      res.write(JSON.stringify({ message: "route not found" }));
-      res.end();
-    }
-  })
-  .listen(PORT, () => console.log(`click to join on http://localhost:${PORT}`));
+const ProductsController = require("./controllers/products.controllers");
+const PORT = 3000;
+const server = http.createServer((req, res) => {
+  // => (Find )
+  if (req.url == "/api/products") {
+    ProductsController.get(req, res);
+  } else {
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.write(JSON.stringify({ message: "route not found" }));
+    res.end();
+  }
+});
+server.listen(PORT, () =>
+  console.log(`click to join on http://localhost:${PORT}`)
+);
+ 
