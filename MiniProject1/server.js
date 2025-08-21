b58d88b456c568d36985e24768e051aebff264ba
omidreza <afifiomid@yahoo.com>
2025-08-21 16:48:42 +0330

    .error handler

--- MiniProject1/server.js
+++ MiniProject1/server.js
@@ -1,18 +1,18 @@
 const http = require("http");
 const products = require("./data/products");
 const ProductsController = require("./controllers/products.controllers");
+const errorHandler = require("./controllers/errorHandler.controller");
 const PORT = 3000;
 const server = http.createServer((req, res) => {
   // => (Find )
   if (req.url == "/api/products") {
     ProductsController.get(req, res);
-  } else {
-    res.writeHead(404, { "Content-Type": "application/json" });
-    res.write(JSON.stringify({ message: "route not found" }));
-    res.end();
+  } else if (req.url.match(/\/api\/products\/[0-9]+/)) {
+   
+  }else {
+    
   }
 });
 server.listen(PORT, () =>
   console.log(`click to join on http://localhost:${PORT}`)
 );
- 
