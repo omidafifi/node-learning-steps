8bf54f3e6c72bf0b7ffb51f78cfc8e6ad00aa74e
omidreza <afifiomid@yahoo.com>
2025-09-23 12:29:04 +0330

    fs modules

--- lesson-06/utils/app.js
+++ lesson-06/utils/app.js
@@ -1,13 +1,20 @@
-// utils/logger.js باید به صورت ES Module باشد
+// utils/logger.js
 export function logger(message) {
   console.log("LOG:", message);
 }
 
 // main.js
 import { logger } from "./utils/logger.js";
+import fs from "fs";
 
 class Person {
-  constructor(age = 34, firstName = "Omidreza", lastName = "", phoneNumber = "", adresse = "") {
+  constructor(
+    age = 34,
+    firstName = "Omidreza",
+    lastName = "",
+    phoneNumber = "",
+    adresse = ""
+  ) {
     this.age = age;
     this.firstName = firstName;
     this.lastName = lastName;
@@ -15,6 +22,23 @@ class Person {
     this.adresse = adresse;
   }
 }
+
+// خواندن فایل به صورت همزمان
+try {
+  const data = fs.readFileSync("example.txt", "utf8");
+  console.log("Synchronous read:", data);
+} catch (error) {
+  console.error("Error reading file synchronously:", error);
+}
+
+// خواندن فایل به صورت غیرهمزمان
+fs.readFile("example.txt", "utf8", (err, data) => {
+  if (err) {
+    console.error("Error reading file asynchronously:", err);
+    return;
+  }
+  console.log("Asynchronously read callback:", data);
+});
 
 const person = new Person();
 logger("Hi Omidreza");
