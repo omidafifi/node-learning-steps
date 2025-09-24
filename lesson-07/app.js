d62acb3f1bb4548212679c410455375ba2782a50
omidreza <afifiomid@yahoo.com>
2025-09-24 18:03:18 +0330

    fs modules

--- lesson-07/app.js
+++ lesson-07/app.js
@@ -1,36 +1,62 @@
-//creat directory
-const { isUtf8 } = require("buffer");
-const fs = require(`fs`);
+// create directory
+const fs = require("fs");
 const path = require("path");
+
 // ساخت پوشه توسط ماژول fs
-fs.mkdir(path.join(__dirname, `test`), {}, (err) => {
+fs.mkdir(path.join(__dirname, "test"), {}, (err) => {
   if (err) {
-    console.log(`Folder created....`);
+    console.log("خطا در ساخت پوشه:", err);
   } else {
-    console.log(`somthing is wrong`);
+    console.log("پوشه ساخته شد ✅");
   }
 });
-//write file
-fs.writeFile(path.join(__dirname, `test`, `test.txt`), `utf8`, (err) => {
+
+// write file
+fs.writeFile(
+  path.join(__dirname, "test", "test.txt"),
+  "سلام دنیا",
+  "utf8",
+  (err) => {
+    if (err) {
+      console.log("خطا در نوشتن فایل:", err);
+    } else {
+      console.log("فایل ساخته/بازنویسی شد ✅");
+    }
+  }
+);
+
+// append file
+fs.appendFile(
+  path.join(__dirname, "test", "test.txt"),
+  "\nاین متن به انتها اضافه شد",
+  "utf8",
+  (err) => {
+    if (err) {
+      console.log("خطا در افزودن متن:", err);
+    } else {
+      console.log("متن به فایل اضافه شد ✅");
+    }
+  }
+);
+
+// read file
+fs.readFile(path.join(__dirname, "test", "test.txt"), "utf8", (err, data) => {
   if (err) {
-    console.log("File witten...");
+    console.log("خطا در خواندن فایل:", err);
   } else {
-    console.log("somthins is error");
+    console.log("محتوای فایل:", data);
   }
 });
-//append file
-fs.appendFile(path.join(__dirname, `test`, `test.txt`), `utf8`, (err) => {
-  if (err) {
-    console.log("File witten...");
-  } else {
-    console.log("somthins is error");
+
+// rename file
+fs.rename(
+  path.join(__dirname, "test", "test.txt"),
+  path.join(__dirname, "test", "newName.txt"),
+  (err) => {
+    if (err) {
+      console.log("خطا در تغییر نام:", err);
+    } else {
+      console.log("نام فایل تغییر کرد ✅");
+    }
   }
-});
-//read file
-fs.readFile(path.join(__dirname), `utf8`, (err, data) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(data);
-  }
-});
+);
