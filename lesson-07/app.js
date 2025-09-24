e115ff50acaa600641bb1d7ddcb2e016eceba8a3
omidreza <afifiomid@yahoo.com>
2025-09-24 12:42:02 +0330

    test again

--- lesson-07/app.js
+++ lesson-07/app.js
@@ -1,10 +1,8 @@
+//creat directory
 const fs = require(`fs`);
-const path = require(`path`);
+const path = require("path");
 
-fs.mkdir(path.join(`test`), {}, (err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(`Foledr created`);
-  }
+fs.mkdir(path.join(__dirname, `test`), {}, (err) => {
+  console.log(err);
 });
+ 
