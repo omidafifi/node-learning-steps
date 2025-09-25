f202c88830bd9b40a97074141374be8a833e2944
omidreza <afifiomid@yahoo.com>
2025-09-25 13:52:22 +0330

    //Logical CPU Core Info

--- lesson-08/app.js
+++ lesson-08/app.js
@@ -1,7 +1,10 @@
 const os = require(`os`);
 
 //Type
-// console.log(os.type());
+console.log(os.type());
 
 // CPU Arch
 console.log(os.arch());
+
+//Logical CPU Core Info 
+console.log(os.cpus());
