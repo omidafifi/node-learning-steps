4f8dd8cee9e0d62a4a73da5b27a4afcdb3aec092
omidreza <afifiomid@yahoo.com>
2025-09-28 15:14:46 +0330

    server.js

--- lesson-12/data/server.js
+++ lesson-12/data/server.js
@@ -1,1 +1,10 @@
-const 
+const http = require(`http`);
+const PORT = 3000;
+const server = http
+  .createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end();
+  })
+  .listen(`${PORT}`, () => {
+    console.log(`Server runing on http://localhost${PORT}`);
+  });
